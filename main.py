d2ffe4c6baaef0ae1013412d837c4369b787d6cd
davidpazukshevchenko <useplay132010@gmail.com>
2023-12-02 12:05:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import datetime
+
+from bs4 import BeautifulSoup
+
+import requests
+
+import sqlite3
+
+conn = sqlite3.connect('weather.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS weather(date_time TEXT, temperature REAL)''')
+url = 'https://ua.sinoptik.ua/погода-львів'
+response = requests.get(url)
+if response.status_code == 200:
+   html = response.content
+else:
+   print('Не вдалося отримати сторінку')
+
+soup = BeautifulSoup(html, 'html.parser')
+today_weather = soup.find('div', {'class': 'weatherToday'}).find('div', {'class': 'temperature'}).text
+today_temperature = int(today_weather.split('°')[0])
+print(f"Температура сьогодні: {today_temperature}°C")
+
+temperature = today_temperature
+date_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+conn = sqlite3.connect('weather.db')
+c = conn.cursor()
+c.execute("INSERT INTO weather VALUES (?, ?)", (date_time, temperature))
